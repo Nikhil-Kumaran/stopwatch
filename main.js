34a396cf089789d0757f3cae673e2da22faee5aa
Nikhil-Kumaran <nikhil.kumaran.2011@gmail.com>
2020-03-01 14:32:05 +0530

    now backspace also resets the stopwatch as Mac has backspace key only not delete key :(

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-var timing = false;
-var interval;
-var time = 0;
-var count = document.getElementById("time");
-var i = 0;
-var format = {
-    min : 0,
-    sec : 0,
+let timing = false;
+let interval;
+let time = 0;
+const count = document.getElementById("time");
+let i = 0;
+const format = {
+    min: 0,
+    sec: 0,
     milli: 0
 };
-// var s = document.getElementById("s");
-// var r = document.getElementById("r");
-// var t = document.getElementById("t");
-// var past = document.getElementById("past");
 
-
-function startstop(){
+function startStop(){
     if(!timing) {
         interval = setInterval(function () {
             time += 0.01;
@@ -32,35 +27,22 @@ function reset(){
     time = 0;
     count.innerHTML = "0";
     timing = false;
-    //past.innerHTML = "";
 }
-
-// function record(){
-//     var node = document.createElement("li");
-//     var textnode=document.createTextNode(count.innerHTML);
-//     node.appendChild(textnode);
-//     past.appendChild(node);
-// }
-
-// s.addEventListener("click",startstop);//not s.addEventListener("click",startstop());
-// r.addEventListener("click",reset);
-// t.addEventListener("click",record);
 
 document.addEventListener('keydown',function(event){
     i = i+1;
-    if(event.keyCode === 32){
-        
-        startstop();
+    console.log(event.code, event.keyCode)
+    if(event.keyCode === 32 || event.code === 'Space'){
+        startStop();
         if(i % 2 == 0){
-            format.min = Math.floor(Math.floor(time.toFixed(2))/60);
-            format.sec = Math.floor(time.toFixed(2))%60;
+            format.min = Math.floor(Math.floor(Number.parseFloat(time.toFixed(2)))/60);
+            format.sec = Math.floor(Number.parseFloat(time.toFixed(2)))%60;
             format.milli =  (time.toFixed(2) + "").split(".")[1];
-            console.log(Math.floor(Math.floor(time.toFixed(2))/60)+" "+Math.floor(time.toFixed(2))%60+" "+ format.milli);
-            
+
             document.getElementById("format").innerHTML = format.min + " : " + format.sec + " : " + format.milli;
         }
     }
-    else if(event.keyCode === 46){
+    else if(event.keyCode === 46 || event.code === 'Delete' || event.keyCode === 8 || event.code === 'Backspace'){
         i = 0;
         document.getElementById("format").innerHTML = 0 + " : " + 0 + " : " + 0;
         reset();
